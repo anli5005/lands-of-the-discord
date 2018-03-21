a67ece079bddaaf30394ecc834956381b5bfd9bf
Anthony Li <li.anthony917@gmail.com>
2018-03-21 11:21:24 -0400

    Add Mongo support

--- bot/plugins/bot.py
+++ bot/plugins/bot.py
@@ -1,6 +1,18 @@
-from disco.bot import Plugin
+import json
+from pymongo import MongoClient
+from disco.bot import Plugin, Config
 
 class LOTDBotPlugin(Plugin):
+    mongoURL = "mongodb://localhost:27017"
+
+    def load(self, ctx):
+        super(LOTDBotPlugin, self).load(ctx)
+        with open("config.json") as f:
+            config = json.loads(f.read())
+            self.mongoURL = config["mongo"]
+        self.client = MongoClient(self.mongoURL)
+        self.db = self.client.lotd
+
     @Plugin.command("ping")
     def command_ping(self, event):
         event.msg.reply("Pong!")
