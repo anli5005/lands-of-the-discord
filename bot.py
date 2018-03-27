cffc0a0b652532d58089494e0a4097e9c6250e30
Anthony Li <li.anthony917@gmail.com>
2018-03-26 22:03:09 -0400

    Switch to discord.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+import asyncio
+import discord
+import json
+from pymongo import MongoClient
+
+from xp import XPManager
+
+mongoURL = "mongodb://localhost:27017"
+token = "SEE-CONFIG-JSON"
+with open("config.json") as f:
+    config = json.loads(f.read())
+    mongoURL = config["mongo"]
+    token = config["token"]
+client = MongoClient(mongoURL)
+db = client.lotd
+
+bot = discord.Client()
+xp = XPManager(db)
+
+@bot.event
+async def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("------")
+
+@bot.event
+async def on_message(message):
+    if message.content == "!ping":
+        await bot.send_message(message.channel, "Pong!")
+    elif message.content == "!instantxp":
+        await bot.send_message(message.channel, "Gave you 100 XP instantly")
+    elif message.content == "!xp":
+        await bot.send_message(message.channel, "You have {} XP.".format(xp.getXP(message.author.id)))
+
+bot.run(token)
---
+++ xp.py
@@ -0,0 +1,5 @@
+from manager import Manager
+
+class XPManager(Manager):
+    def getXP(self, user):
+        return 10000
